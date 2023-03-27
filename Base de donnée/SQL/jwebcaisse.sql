550a076af08c62c7fcbdc34cd0b952f2a13bdee7
Maxime C <maxime_curtil19@orange.fr>
2023-03-27 10:34:37 +0200

    Update jwebcaisse.sql

--- Base de donnée/SQL/jwebcaisse.sql
+++ Base de donnée/SQL/jwebcaisse.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `jwebcaisse`
 --
-CREATE DATABASE IF NOT EXISTS `jwebcaisse` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `jwebcaisse`;
+CREATE DATABASE IF NOT EXISTS `webcaisse_ap2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `webcaisse_ap2`;
 
 -- --------------------------------------------------------
 
